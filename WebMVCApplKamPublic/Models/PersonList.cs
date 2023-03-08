d8d12a5ecd57dcc5ab42bf79b55b2be43d209c18
Kamil <k>
2023-03-08 19:12:54 +0100

    Založení třídy seznam osob pro DB tabulku

---
+++ WebMVCApplKamPublic/Models/PersonList.cs
@@ -0,0 +1,13 @@
+﻿namespace WebMVCApplKamPublic.Models
+{
+    public class PersonList
+    {
+        /*
+        
+        Úkolem je naprogramovat webovou aplikaci v ASP.NET MVC, která umožňuje vložit do 
+        databáze Osobu (Jméno, Příjmení, E-mailová adresa). 
+        Aplikace zároveň umožňuje Osobu v databázi upravit a smazat.
+
+         */
+    }
+}
